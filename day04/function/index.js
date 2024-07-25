d6454a88d4eb8cb3cbbeef27585da1addf4dbba5
cucumbermother <duwkrladbsk2@naver.com>
2024-07-25 14:53:44 +0900

    Add files via upload

---
+++ day04/function/index.js
@@ -0,0 +1,85 @@
+// function [함수]
+
+
+
+
+
+
+// function add100(x) {
+
+// return x + 100;
+
+// }
+
+
+// function minus100(x) {
+
+// return x - 100;
+
+// }
+
+
+// function wonyongthinking(x) {
+
+// return x + " 럭키비키 잖아 ";
+
+// }
+
+
+// function test (x,y) {
+//  return (x + y) ** 2;
+
+// }
+
+// function number (x)  {
+
+// if (x % 2 == 1) { return "홀수"}
+// else {
+// return "짝수";
+
+// }
+
+// }
+
+
+// function test1 (x,y) {
+
+//     return x > y ? x : y;
+
+// }
+
+
+// const a = test1(11,20);
+
+
+
+// alert(a)
+
+
+
+
+function test2 (x,y){
+
+return x == y ? "같아요" : "달라요"
+
+}
+
+
+test2(100, 200);
+test2(200, 300);
+test2(100, 100);
+
+// prompt("너가 쓰고 싶은말 쓰셈");
+// alert("비상 비상 비상");
+
+
+// const a = add100(3000); //3100
+// const b = minus(1000); // 900
+// const c = wonyongthinking("수업을 하고 있는데 마침 휴강이라서");
+
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+
---
+++ day04/review/index.js
@@ -0,0 +1,17 @@
+const bus = Number(prompt("[마을버스:1500,고속버스:2000, 우등버스:3000] 버스 종류 입력"))
+const age = Number(prompt("나이를 적어주세요"))
+const bus_fee = [1500,2000,3000];
+const bus_name = ["마을버스","고속버스","우등버스"];
+
+if (age <= 13){
+    console.log(`${bus_name[bus]} 0원 부탁드립니다`)}
+
+    else if(65 <= age) {
+        console.log(`${bus_name[bus]} ${bus__fee[bus]*0.7}원 결제부탁드립니다`)
+    }
+
+    
+    else {
+
+        console.log(`${bus_name[bus]} ${bus__fee[bus]}원 결제부탁드립니다`)
+    }
